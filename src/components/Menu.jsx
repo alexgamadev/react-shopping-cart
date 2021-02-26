5e246bd39d3dd578b3ec92c23ab59489b10f410c
alexgamadev <alexgama.97@outlook.com>
2021-02-26 22:39:22 +0000

    Imported react-spring and added for menu transition

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import {useSpring, animated, config} from 'react-spring';
 import Icon from './Icon';
 import { Close as CloseIcon } from '@styled-icons/ionicons-solid/Close';
 
-const StyledMenu = styled.div`
+export default function Menu(props) {
+    const { isOpen, closeMenu } = props;
+    const openAnim = useSpring({
+        transform: isOpen ? `translateX(0)` : 'translateX(-100vw)',
+        config: {...config.stiff, clamp: true}
+    })
+    console.log(props);
+    return (
+        <StyledMenu isOpen={isOpen} style={openAnim}>
+            <MenuList>
+                <StyledCloseIcon size={40} onClick={() => closeMenu()}/>
+                <MenuLink>Home</MenuLink>
+                <MenuLink>Shop</MenuLink>
+                <MenuLink>About</MenuLink>
+            </MenuList>
+        </StyledMenu>
+    )
+}
+
+const StyledMenu = styled(animated.div)`
     position: absolute;
     top: 0px;
     left: 0px;
     height: 100vh;
-    width: 80vw;
+    width: 100vw;
     background-color: #0f1520;
 
     transform: ${props => props.isOpen ? 'translateX(0)' : 'translateX(-100vw)'};
@@ -36,19 +56,3 @@ const StyledCloseIcon = styled(CloseIcon)`
         background-color: #182640;
     }
 `;
-
-
-export default function Menu(props) {
-    const { isOpen, closeMenu } = props;
-    console.log(props);
-    return (
-        <StyledMenu isOpen={isOpen}>
-            <MenuList>
-                <StyledCloseIcon size={40} onClick={() => closeMenu()}/>
-                <MenuLink>Home</MenuLink>
-                <MenuLink>Shop</MenuLink>
-                <MenuLink>About</MenuLink>
-            </MenuList>
-        </StyledMenu>
-    )
-}
