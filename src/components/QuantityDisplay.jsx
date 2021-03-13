2400ebb1a2468a2a5718b19eb60d93e19dec7f18
alexgamadev <alexgama.97@outlook.com>
2021-03-13 11:29:12 +0000

    Added quantity display component with dispatch calls to basket

---
+++ src/components/QuantityDisplay.jsx
@@ -0,0 +1,20 @@
+import React, {useContext} from 'react'
+import { BasketContext } from '../reducers/BasketStore'
+
+export default function QuantityDisplay(props) {
+    const {dispatch} = useContext(BasketContext);
+    const {quantity, id} = props;
+    console.log(quantity);
+
+    function adjustQuantity(change) {
+        dispatch({type:'adjust-quantity', payload: {change, id}});
+    }
+
+    return (
+        <div>
+            <button onClick={() => adjustQuantity(-1)}>-</button>
+            {quantity}
+            <button onClick={() => adjustQuantity(1)}>+</button>
+        </div>
+    )
+}
