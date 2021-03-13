bdeefc2fa15e6c02bd18e6f8c0c9dfb4518026c6
alexgamadev <alexgama.97@outlook.com>
2021-03-13 11:31:24 +0000

    Implemented adjust-quantity action for reducer

--- src/reducers/BasketStore.js
+++ src/reducers/BasketStore.js
@@ -3,33 +3,52 @@ import React, { useReducer } from "react"
 function basketReducer(basket, action) {
     const {payload} = action;
     switch(action.type) {
-        case 'addToBasket':
+        case 'add-to-basket':
             return addToBasket(basket, payload.item);
-            
+        case 'adjust-quantity':
+            return changeQuantity(basket, payload);
         default: return basket;
+    }
+}
+
+function changeQuantity(basket, payload) {
+    console.log(payload)
+    const {change, id} = payload;
+    const itemIndex = basket.findIndex(item => item.id === id);
+    if(itemIndex <= -1) {
+        return basket;
+    } else {
+        return basket.map(item => {
+            if(item.id === id) {
+                return {
+                    ...item,
+                    quantity: item.quantity + change
+                };
+            } else {
+                return item;
+            }
+        });
     }
 }
 
 function addToBasket(basket, newItem) {
     const itemIndex = basket.findIndex(item => item.id === newItem.id);
-    console.log(itemIndex);
     if(itemIndex > -1) {
-        basket[itemIndex].quantity++;
-        return basket;
+        changeQuantity(basket, {change: 1, id:itemIndex})
     } else {
         newItem.quantity = 1;
         return [...basket, newItem]; 
     }
 }
 
+export const BasketContext = React.createContext();
+
 export default function BasketStore({children}) {
     const [basket, dispatch] = useReducer(basketReducer, []);
 
     return (
-        <BasketContext.Provider value={[basket, dispatch]}>
+        <BasketContext.Provider value={{basket, dispatch}}>
             {children}
         </BasketContext.Provider>
     );
 }
-
-export const BasketContext = React.createContext([]);
