1950ca1cc06c2ed2068545265248be768764f95e
alexgamadev <alexgama.97@outlook.com>
2021-03-14 21:20:22 +0000

    Refactored code to allow for displaying a message when no results are found

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 import Navbar from '../components/Navbar';
 import styled from 'styled-components';
 import ShopCard from '../components/ShopCard';
@@ -14,15 +14,15 @@ const StyledSection = styled.section`
     margin: 20px;
 `;
 
-function useQuery() {
-    return new URLSearchParams(useLocation().search);
-}
-
 export default function Shop() {
     const [data] = useFetchAPI('https://fakestoreapi.com/products');
+    const [items, setItems] = useState(0);
+    const [searchParams, setSearchParams] = useState('');
     const {dispatch} = useContext(BasketContext);
     const notyf = useContext(NotyfContext);
     const history = useHistory();
+    const location = useLocation();
+    const useQuery = useCallback(() => new URLSearchParams(searchParams), [searchParams]);
     const urlQuery = useQuery();
 
     function addToBasket(item) {
@@ -32,36 +32,36 @@ export default function Shop() {
     }
 
     function search(input) {
-        console.log(input);
         const searchQuery = input.length > 0 ? `?search=${input}` : '';
         history.push(`/shop${searchQuery}`);
     }
 
+    useEffect(() => {
+        setSearchParams(location.search);
+    }, [location.search]);
 
-    function displayItems(item) {
-        console.log(urlQuery);
+    useEffect(() => {
+        setItems(data?.flatMap(getItems));
+    }, [data, searchParams]);
+
+    function getItems(item) {
         const searchTerm = urlQuery.get('search');
         const categoryTerm = urlQuery.get('category');
+
         if(searchTerm !== null){
             if(item.title.toUpperCase().includes(searchTerm.toUpperCase()) ||
-             item.category.toUpperCase().includes(searchTerm.toUpperCase())){
-                return (<ShopCard key={item.id} item={item}>
-                        <BasketButton onClick={() => addToBasket(item)}>Add To Basket</BasketButton>
-                        </ShopCard>
-                )
+             item.category.toUpperCase().includes(searchTerm.toUpperCase())) {
+                return item;
             }
         } else if(categoryTerm !== null){
             if(item.category === categoryTerm){
-                return (<ShopCard key={item.id} item={item}>
-                        <BasketButton onClick={() => addToBasket(item)}>Add To Basket</BasketButton>
-                        </ShopCard>
-                )
+                return item;
             }
         } else {
-            return (<ShopCard key={item.id} item={item}>
-                <BasketButton onClick={() => addToBasket(item)}>Add To Basket</BasketButton>
-                </ShopCard>)
+            return item;
         }
+
+        return [];
     }
 
     return (
@@ -71,11 +71,19 @@ export default function Shop() {
             {data ? 
             (  
                 <StyledSection>
-                    {data && 
+                    {items?.length > 0 ? (
                         <ResponsiveGrid>
-                            {data?.map(displayItems)}
-                        </ResponsiveGrid>                
-                    }   
+                            {items?.map(item => {
+                                return (
+                                    <ShopCard key={item.id} item={item}>
+                                        <BasketButton onClick={() => addToBasket(item)}>Add To Basket</BasketButton>
+                                    </ShopCard>
+                                );
+                            })}
+                        </ResponsiveGrid>   
+                    ) : (
+                        <h1>No results found</h1>
+                    )}
                 </StyledSection>
             ) :
             (
